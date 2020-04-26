f67d1627b1d2f611e53d5c02b6cbab79da504bec
Watit Thammarat <tongnakub@hotmail.com>
2020-04-26 11:30:50 +0700

    end of section 7

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from './CustomError';
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connecting to database';
+  statusCode = 500;
+
+  constructor() {
+    super('Error connecting to DB');
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { errorHandler } from './middlewares/erorr-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+app.use(currentUserRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.all('*', async () => {
+  throw new NotFoundError();
 });
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');

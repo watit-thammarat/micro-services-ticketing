bbf27c6573c762ef5af129dbac8bd1ba26df52a8
Watit Thammarat <tongnakub@hotmail.com>
2020-05-26 16:13:29 +0700

    update comment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 
 const start = async () => {
   try {
-    //comment
+    //comment.
     if (!process.env.JWT_KEY) {
       throw new Error('JWT_KEY must be defined');
     }
